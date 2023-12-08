64c8fec211b0085fbab5937d6468e26e885e3cc8
ZoOoOoP <32754782+ZoOoOoP@users.noreply.github.com>
2023-12-07 22:17:39 -0400

    Create JVC - Fixation Dérèglement Quantique.js

---
+++ JVC - Fixation Dérèglement Quantique.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         FIXATION DU DEREGLEMENT QUANTIQUE
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  FIXATION DU DEREGLEMENT QUANTIQUE QUI SUPPRIME LE POINT DU I APRES UN F
+// @author       You
+// @match        *://*.jeuxvideo.com/*
+// @grant        none
+// @run-at       document-end
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Fonction pour ajouter du CSS
+    function addGlobalStyle(css) {
+        var head, style;
+        head = document.getElementsByTagName('head')[0];
+        if (!head) { return; }
+        style = document.createElement('style');
+        style.type = 'text/css';
+        style.innerHTML = css;
+        head.appendChild(style);
+    }
+
+    // Ajouter le CSS personnalisé
+    addGlobalStyle('* { font-variant-ligatures: none; }');
+})();
